1dc23fc3f276b518e86f16d4ae3a36c3289cdc14
FredBettecher <fredbettecher@gmail.com>
2022-10-24 17:30:26 -0300

    jogo funcional e responsivo

--- script/script.js
+++ script/script.js
@@ -1,20 +1,25 @@
 // definindo variaveis
 const cards = document.querySelectorAll('.memory-card');
 let cardFlipped = false; // condição que verifica se a carta foi virada
-let cardOne, cardTwo;
+let cardOne, cardTwo; // variáveis que vão ser usadas para comparar as cartas
 let lockGame = false; // condição que bloqueia a interação com o jogo (usada para evitar bugs)
-let cardsArray = [];
+let count = 0; // contador de jogadas
+let qtdOfCards = 0; // quantidade de cartas
+let turnedPairs = 0; // contador de pares virados
 
 // função que controla as cartas 
-function flipCard(card){
-    if(lockGame) return;
+function flipCard(card){    
+    if(lockGame) return; // impede interações com a tela enquanto a animação da carta ainda não terminou
 
-    if(card === cardOne) return;
+    if(card === cardOne) return; // impede interações com a tela se clicar na mesma carta
 
-    card.classList.add('flip');
+    card.classList.add('flip'); // tudo normal por aqui :)
 
     cardTwo = card;
 
+    count++; // adiciona uma jogada
+    console.log("Nº de jogadas: " + count);
+    
     // checando se uma carta foi virada
     if(cardFlipped === false){
         cardFlipped = true;
@@ -29,6 +34,8 @@ function flipCard(card){
     if(cardOne.dataset.match === cardTwo.dataset.match){
         cardOne.removeAttribute('onclick');
         cardTwo.removeAttribute('onclick');
+        turnedPairs++;
+        console.log("Pares virados: " + turnedPairs);
         cardFlipped = false;
         lockGame = false;
         cardOne = null;
@@ -47,7 +54,7 @@ function flipCard(card){
     }
 }
 
-// funcao que embaralha as cartas
+// função que embaralha as cartas
 function mixCards(){
     cards.forEach(card => {
         let position = Math.floor(Math.random() * 14);
@@ -55,4 +62,31 @@ function mixCards(){
     });
 }
 
-mixCards();
+// função que seleciona o número de cartas
+function quantityOfCards(){
+    while((qtdOfCards < 4 || qtdOfCards > 14) || (qtdOfCards % 2 !== 0)){
+        qtdOfCards = prompt("Escolhar um número par de cartas entre 4 e 14");
+    }
+
+    if((qtdOfCards >= 4 || qtdOfCards <= 14) || (qtdOfCards % 2 === 0)){
+        for(let i = 0; i < qtdOfCards; i++){
+            document.getElementById(`card${i}`).style.display = 'block';
+        }
+    }
+}
+
+function endGame(){
+    if((turnedPairs*2) === qtdOfCards){
+        alert(`Você ganhou em ${count} jogadas!`);
+        console.log(`Você ganhou em ${count} jogadas!`);
+        }
+}
+
+// função que avisa quando o jogo termina
+function main(){
+    mixCards();
+    quantityOfCards();
+    endGame();
+}
+
+main();
